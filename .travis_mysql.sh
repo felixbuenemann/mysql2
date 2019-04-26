a851d98312c70bf89e612a3bdddc9a83c8ccdb8c
Felix Bünemann <buenemann@louis.info>
2019-04-26 03:14:58 +0200

    Update Travis CI to Trusty / Test MySQL 8.0
    * Remove hardcoded dist names from scripts, reduce dependencies
    * Update SSL script to force SHA1 for yaSSL on Trusty
    * Update MySQL install script to support MySQL 8.0 install
    * Add DB=mysql8 to build matrix
    * Switch from deprecated precise to trusty dist
    * Fix MariaDB downgrade 5.6 -> 5.5 on Trusty

---
+++ .travis_mysql.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+service mysql stop
+
+apt-get purge '^mysql*' 'libmysql*'
+apt-get autoclean
+
+rm -rf /var/lib/mysql /var/log/mysql
+
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x8C718D3B5072E1F5
+
+. /etc/lsb-release
+
+if [[ x$1 = xmysql56 ]]; then
+  echo "deb http://repo.mysql.com/apt/ubuntu $DISTRIB_CODENAME mysql-5.6" >> /etc/apt/sources.list
+elif [[ x$1 = xmysql57 ]]; then
+  echo "deb http://repo.mysql.com/apt/ubuntu $DISTRIB_CODENAME mysql-5.7" >> /etc/apt/sources.list
+elif [[ x$1 = xmysql8 ]]; then
+  echo "deb http://repo.mysql.com/apt/ubuntu $DISTRIB_CODENAME mysql-8.0" >> /etc/apt/sources.list
+fi
+
+apt-get update
+apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -y install mysql-server libmysqlclient-dev
+
+# disable socket auth on mysql-community-server
+echo "UPDATE user SET plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root mysql
+
+service mysql restart
